adfe01fd8124c02c36f1fa559bbed5c2cdb9d421
Mohsin Nawaz <84810473+mohsini75@users.noreply.github.com>
2022-01-05 23:08:52 +0500

    mohsin

---
+++ build/app/generated/source/buildConfig/debug/com/example/easybus_web/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.easybus_web;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.easybus_web";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
